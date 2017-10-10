2afe04e620a3b2c078a1321b615063461fa92976
Mikhail Zyatin <mikhail.zyatin@gmail.com>
2017-10-10 21:36:01 +0300

    Refactor image processing.

--- acoustic_sight/webcam.py
+++ acoustic_sight/webcam.py
@@ -15,57 +15,99 @@ from logger import logger
 Synth, init_audio = sound_drivers.get_driver(sound_drivers.SUPER_COLLIDER)
 
 
-def process_frame(frame, side_in, side_out):
-    (height, width, *_) = frame.shape
+class FrameProcessor:
+    def __init__(self, side_in, side_out, buffer_size=2):
+        self.side_in = side_in
+        self.side_out = side_out
+        self.buffer_size = buffer_size
+        self.frame_buffer = list()
 
-    # Transformations
-    downsample = lambda img: cv2.resize(img, (side_in, side_in))
-    sharp = lambda img: cv2.filter2D(img, -1, numpy.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]))
-    gaussian_blur = lambda img: cv2.GaussianBlur(img, (5, 5), 0)
-    median_blur = lambda img: cv2.medianBlur(img, 5)
-    denoising = lambda img: cv2.fastNlMeansDenoising(img, h=7, templateWindowSize=7, searchWindowSize=21)
-    canny_edges = lambda img: cv2.Canny(img, 50, 100)
-    laplasian = lambda img: cv2.Laplacian(img, cv2.CV_64F)
-    grayscale = lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    mirror = lambda img: cv2.flip(img, 1)
-    upsample = lambda img: cv2.resize(img, (side_out, side_out))
+    def _store_frame(self, frame):
+        self.frame_buffer.append(frame)
+        if len(self.frame_buffer) > self.buffer_size:
+            self.frame_buffer = self.frame_buffer[1:]
 
+    @staticmethod
+    def sharp(img):
+        return cv2.filter2D(img, -1, numpy.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]))
+
+    @staticmethod
+    def gaussian_blur(img):
+        return cv2.GaussianBlur(img, (5, 5), 0)
+
+    @staticmethod
+    def median_blur(img):
+        return cv2.medianBlur(img, 5)
+
+    @staticmethod
+    def denoising(img):
+        return cv2.fastNlMeansDenoising(img, h=7, templateWindowSize=7, searchWindowSize=21)
+
+    @staticmethod
+    def canny_edges(img):
+        return cv2.Canny(img, 50, 100)
+
+    @staticmethod
+    def laplasian(img):
+        return cv2.Laplacian(img, cv2.CV_64F)
+
+    @staticmethod
+    def grayscale(img):
+        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    @staticmethod
+    def mirror(img):
+        return cv2.flip(img, 1)
+
+    @staticmethod
     def square_crop(img):
+        (height, width, *_) = img.shape
+
         side = min((width, height))
         left = (width - side) // 2
         right = left + side
         top = (height - side) // 2
         bottom = top + side
+
         return img[top:bottom, left:right]
 
-    def apply_chain(img, transform_chain):
+    def downsample(self, img):
+        return cv2.resize(img, (self.side_in, self.side_in))
+
+    def upsample(self, img):
+        return cv2.resize(img, (self.side_out, self.side_out))
+
+    def apply_chain(self, img, transform_chain):
         for transform in transform_chain:
             img = transform(img)
         return img
 
-    # Create base image
-    base_transformations = [
-        grayscale,
-        square_crop,
-        mirror,
-    ]
-    base = apply_chain(frame, base_transformations)
+    def process_frame(self, frame):
+        self._store_frame(frame)
 
-    # Create sound input image
-    sound_input = apply_chain(base, [
-        gaussian_blur,
-        downsample,
-        denoising,
-        # median_blur,
-        # sharp,
-        # laplasian,
-        # canny_edges,
-    ])
+        # Create base image
+        base_transformations = [
+            self.grayscale,
+            self.square_crop,
+            self.mirror,
+        ]
+        base = self.apply_chain(frame, base_transformations)
 
-    # Sample up to [side_out x side_out] for better preview
-    upsampled = upsample(sound_input)
+        # Create sound input image
+        sound_input = self.apply_chain(base, [
+            self.gaussian_blur,
+            self.downsample,
+            self.denoising,
+            # self.median_blur,
+            # self.sharp,
+            # self.laplasian,
+            # self.canny_edges,
+        ])
 
-    return sound_input, upsampled, base
+        # Sample up to [side_out x side_out] for better preview
+        upsampled = self.upsample(sound_input)
+
+        return sound_input, upsampled, base
 
 
 class Sonificator:
@@ -98,6 +140,7 @@ class WebcamApp:
         self.octaves = octaves
         self.tone_shift = tone_shift
         self.sonify = sonify
+        self.frame_processor = FrameProcessor(self.side_in, self.side_out)
 
     def _init_sonificator(self):
         if self.sonify:
@@ -116,7 +159,7 @@ class WebcamApp:
         while True:
             # Capture frame-by-frame
             ret, frame = cap.read()
-            sound_input, upsampled, cropped = process_frame(frame, self.side_in, self.side_out)
+            sound_input, upsampled, cropped = self.frame_processor.process_frame(frame)
 
             self._sonify(sound_input)
 
